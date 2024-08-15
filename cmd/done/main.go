77553aff46915fec22542392fb359af3031a2b59
Parikshit Misra <9302404+perryizgr8@users.noreply.github.com>
2024-08-15 13:41:22 +0530

    templates

--- cmd/done/main.go
+++ cmd/done/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/template/html/v2"
 )
+
+type Task struct {
+	Desc string
+	Date time.Time
+}
 
 func main() {
 	// Initialize a new Fiber app
-	app := fiber.New()
+	engine := html.New("./templates", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	app.Get("/marco", func(c fiber.Ctx) error {
 		return c.SendString("polo!")
@@ -17,7 +27,14 @@ func main() {
 	// Define a route for the GET method on the root path '/'
 	app.Get("/", func(c fiber.Ctx) error {
 		// Send a string response to the client
-		return c.SendString("done")
+		return c.Render("index", fiber.Map{
+			"Tasks": []Task{
+				{Desc: "item 1"},
+				{Desc: "item 2"},
+				{Desc: "item 3"},
+				{Desc: "lkolz 4"},
+			},
+		})
 	})
 
 	// Start the server on port 3000
--- go.mod
+++ go.mod
@@ -2,10 +2,15 @@ module github.com/perryizgr8/done
 
 go 1.22.0
 
-require github.com/gofiber/fiber/v3 v3.0.0-beta.3
+require (
+	github.com/gofiber/fiber/v3 v3.0.0-beta.3
+	github.com/gofiber/template/html/v2 v2.1.2
+)
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/gofiber/template v1.8.3 // indirect
+	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
